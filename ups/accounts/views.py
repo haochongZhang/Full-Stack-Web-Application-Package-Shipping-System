3632a1bd683675c48f59d5ac0d4b21ea73cfab61
hz132 <hz132@duke.edu>
2017-04-25 17:50:17 -0400

    changes to login, signup pages

--- ups/accounts/views.py
+++ ups/accounts/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
+def signup(request):
+    if request.method == "POST":
+        if request.POST["password1"] != request.POST["password2"]:
+            return render(request, "accounts/signup.html", {"message": "please enter the same password"})
+        if ',' in request.POST["username"]:
+            return render(request, "accounts/signup.html", {"message": "please donot contain comma in your username"})
+        else:
+            try:
+                User.objects.get(username=request.POST["username"])
+                return render(request, "accounts/signup.html",
+                              {"message": "username has already been taken, try another one"})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST["username"], password=request.POST["password1"],
+                                                email=request.POST["e_mail"])
+                login(request, user)
+                return render(request, "accounts/login.html")
+    else:
+        return render(request, "accounts/signup.html")
+
+
+def Login(request):
+    if request.method == "POST":
+        user = authenticate(username=request.POST["username"], password=request.POST["password"])
+        if user is not None:
+            login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST["next"])
+            return redirect('home')
+        else:
+            return render(request, "accounts/login.html", {"message": "user or password incorrect"})
+    else:
+        return render(request, "accounts/login.html")
+
+
+def Logout(request):
+    if request.method == "POST":
+        logout(request)
+        return render(request, "accounts/login.html")
+
