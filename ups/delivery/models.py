5e5d73e4702f31f688fa2243498f4d415d6efba1
hz132 <hz132@duke.edu>
2017-04-25 16:18:09 -0400

    first trial from pycharm

--- ups/delivery/models.py
+++ ups/delivery/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Package(models.Model):
+    p_id = models.AutoField(primary_key=True) #customed primary key
+    p_name = models.CharField(max_length=100)
+    status = models.CharField(max_length=20)
+    x_loc = models.IntegerField()
+    y_loc = models.IntegerField()
+    user = models.OneToOneField(User)
+
+    def __str__(self):
+        return self.p_name
+
