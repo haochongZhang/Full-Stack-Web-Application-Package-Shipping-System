c02cf57040d1e4a3f6bd0102f84c0a6aa8dd09be
hz132 <hz132@duke.edu>
2017-04-25 17:40:44 -0400

    login, signup pages

---
+++ ups/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+app_name = "accounts"
+
+
+urlpatterns = [
+    url(r'^signup/', views.signup, name='SignUp'),
+    url(r'^login/', views.Login, name='Login')
+]
--- ups/delivery/models.py
+++ ups/delivery/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Package(models.Model):
     p_id = models.AutoField(primary_key=True) #customed primary key
