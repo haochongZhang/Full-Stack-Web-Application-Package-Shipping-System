c60f70cc54979f087429adfd3b51bef527823d08
hz132 <hz132@duke.edu>
2017-04-25 17:55:45 -0400

    changes to login, signup pages

--- ups/ups/urls.py
+++ ups/ups/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include("accounts.urls")),
-    url(r'delivery/', include("delivery.urls")),
+    url(r'^delivery/', include("delivery.urls")),
 ]
