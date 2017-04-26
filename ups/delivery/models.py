2358017aacd5dcfa1dbf979e7d2fb8a9455c034d
Your Name <you@example.com>
2017-04-25 22:00:45 -0400

    models sencond commit

--- ups/delivery/models.py
+++ ups/delivery/models.py
@@ -4,13 +4,21 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Truck(models.Model):
+    truck_id = models.AutoField(primary_key=True) #customed primary key
+    status = models.CharField(max_length=20)
+    x_loc = models.IntegerField()
+    y_loc = models.IntegerField()
+
+    def __str__(self):
+        return self.truck_id    #???
+
 class Package(models.Model):
     package_id = models.AutoField(primary_key=True) #customed primary key
     package_name = models.CharField(max_length=100)
     status = models.CharField(max_length=20) 
-    x_loc = models.IntegerField()
-    y_loc = models.IntegerField()
-    dest_addr = models.CharField(max_length=100)
+    dest_addr_x = models.IntegerField()
+    dest_addr_y = models.IntegerField()
     truck = models.ForeignKey(Truck, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -26,11 +34,3 @@ class Item(models.Model):
     def __str__(self):
         return self.item_name
 
-class Truck(models.Model):
-    truck_id = models.AutoField(primary_key=True) #customed primary key
-    status = models.CharField(max_length=20)
-    x_loc = models.IntegerField()
-    y_loc = models.IntegerField()
-
-    def __str__(self):
-        return self.truck_id
--- ups/ups/settings.py
+++ ups/ups/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'delivery.apps.DeliveryConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
