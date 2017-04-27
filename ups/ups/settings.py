59d3d1e58a86b3efa1e25526301a3c5efb929d5f
Your Name <you@example.com>
2017-04-27 16:32:57 -0400

    sim proto complied

--- ups/ups/settings.py
+++ ups/ups/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'uqg(gw2nptzon_6&@%x-ukuk-j9ga0ymfy%+4w*)^iruhb3t7s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['colab-sbx-pvt-14.oit.duke.edu']
+ALLOWED_HOSTS = ['colab-sbx-pvt-14.oit.duke.edu',
+                 'colab-sbx-pvt-29.oit.duke.edu',
+]
 
 
 # Application definition
