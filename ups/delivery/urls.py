224c89668efd75e364cc27ba8fb613bc3d12b1c4
hz132 <hz132@duke.edu>
2017-04-27 02:38:35 -0400

    good

--- ups/delivery/urls.py
+++ ups/delivery/urls.py
@@ -5,6 +5,6 @@ app_name = "delivery"
 
 urlpatterns = [
     url(r'^searchShipment/', views.searchShipment, name='searchShipment'),
-    url(r'^overviewShipment/', views.overviewShipment, name='overviewShipmet'),
+    url(r'^overviewShipment/', views.overviewShipment, name='overviewShipment'),
     url(r'^detailShipment/(?P<pk>[0-9]+)$', views.detailShipment, name='detailShipment'),
 ]
--- ups/delivery/views.py
+++ ups/delivery/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
 from .models import Shipment, Item
 
@@ -35,17 +35,19 @@ def overviewShipment(request):
         shipments = request.user.shipment_set.all()
         return render(request, "delivery/overviewShipment.html", {"shipments":shipments})
     else:
-        return redirect("Login")
+        return redirect("accounts/Login")
 
 @login_required
 def detailShipment(request, pk):
     if request.method == "POST":
-        if request.POST["xAddr"] and request.POST["yAddr"]:
+        if "xAddr" in request.POST and "yAddr" in request.POST:
             shipment = Shipment.objects.get(shipment_id=pk)
             shipment.dest_addr_x = request.POST["xAddr"]
             shipment.dest_addr_y = request.POST["yAddr"]
-            #shipment.save()
-            return redirect("delivery/overviewShipment")
+            shipment.save()
+            return redirect("/delivery/overviewShipment")
+        else:
+            return redirect("/delivery/overviewShipment")
     else:
         if Shipment.objects.filter(shipment_id=pk).exists() and request.user.is_authenticated():
             shipment = Shipment.objects.get(shipment_id=pk)
