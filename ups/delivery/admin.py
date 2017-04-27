7933686473f00831aa5fc999cc51b44f002b4d09
hz132 <hz132@duke.edu>
2017-04-26 21:58:46 -0400

    changes to search page

--- ups/delivery/admin.py
+++ ups/delivery/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Truck, Shipment, Item
 
 # Register your models here.
+admin.site.register(Truck)
+admin.site.register(Shipment)
+admin.site.register(Item)
